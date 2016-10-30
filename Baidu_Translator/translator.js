8a77c14ca75eb73a2009a7c13cf767d42e7096ee
YG <YG@ACER-5750G>
2016-10-30 23:41:33 +0800

    ju

--- Baidu_Translator/translator.js
+++ Baidu_Translator/translator.js
@@ -1,7 +1,50 @@
-
-$(function () {
-	$(document).keydown(function (event) {
-		console.log(event);
-		console.log("是否Ctrl: " + event.ctrlKey + "; 按键代码： " + event.keyCode);
+function callTranslateApi(originalText, successCallback) {
+	$.ajax({
+		url: "http://fanyi.baidu.com/transapi",
+		type: "POST",
+		dataType: "json",
+		data: {
+			from: "en",
+			to: "zh",
+			source: "txt",
+			query: originalText
+		},
+		success: function (data) {
+			// TODO 处理格式转换
+			var result = data;
+			successCallback(result);
+		},
+		error: function (error) {
+			console.error("百度翻译API调用错误！ 请联系作者 guzhilinghun@qq.com 更新。");
+		}
 	});
-});
+}
+
+function translateAndShow(originalText) {
+	callTranslateApi(originalText, showResult);
+}
+
+function showResult(result) {
+	console.error(result);
+}
+
+function hideResult() {
+
+}
+
+function keypressListener(keyConfig, callback) {
+	$(document).keydown(function (event) {
+		var originalText = window.getSelection().toString()
+		if (event.key == keyConfig.key) {
+			// console.error(event);
+			console.error("待翻译的字段为: " + originalText);
+			callback(originalText);
+			if (1 == 2) {
+				event.preventDefault();
+			}
+		}
+	});
+}
+
+// 注册按键事件
+keypressListener({ key: "`" }, translateAndShow);
